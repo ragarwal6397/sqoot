8e8d8f77de322171c930cef8ced4507d850dbe46
Rajat Agarwal <ragarwal@neighborcommunity.com>
2014-07-27 23:23:40 -0700

    Fix missing comma from previous commit. Signed-off-by: Rajat Agarwal <ragarwal@neighborcommunity.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import sqoot
 
 setup(
     name='sqoot',
-    version='20140727'
+    version='20140727',
     author='Rajat Agarwal',
     author_email='rajatagarwal@alumni.purdue.edu',
     url='https://github.com/ragarwal6397',
